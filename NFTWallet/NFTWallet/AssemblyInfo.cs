2369ed23d288405b1d27cdcf35aba295eadb0d9a
ErickSilva2605 <erick.dev.it@gmail.com>
2022-05-14 22:48:49 -0300

    Adicionando embedded fonts

--- NFTWallet/NFTWallet/AssemblyInfo.cs
+++ NFTWallet/NFTWallet/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("DMSans-Regular.ttf", Alias = "FontRegular")]
+[assembly: ExportFont("DMSans-Medium.ttf", Alias = "FontMedium")]
