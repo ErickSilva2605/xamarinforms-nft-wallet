950804224290e5c205004fb7a8d28a9fe9b90049
ErickSilva2605 <erick.dev.it@gmail.com>
2022-05-16 01:08:04 -0300

    Iniciando implementação de temas dark e light.

---
+++ NFTWallet/NFTWallet/Resources/Theme/BaseTheme.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NFTWallet.Resources.Theme
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BaseTheme : ResourceDictionary
+    {
+        public BaseTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
